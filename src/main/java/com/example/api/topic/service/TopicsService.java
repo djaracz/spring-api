8ee05c6d549b85eeaaaa4183f8b5024d3aad56a1
djaracz <dawid.jaracz@gmail.com>
2018-07-30 23:33:41 +0200

    ceaning

--- src/main/java/com/example/api/topic/service/TopicsService.java
+++ src/main/java/com/example/api/topic/service/TopicsService.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 // Spring treats it as a singleton, and creates instance of it at booting application
 @Service
